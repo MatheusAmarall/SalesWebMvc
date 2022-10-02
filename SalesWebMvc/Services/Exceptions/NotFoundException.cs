d199cd76eafc5bed404f66b256e115845ec1d654
Matheus Amaral <m.amaral8998@gmail.com>
2022-10-02 14:06:16 -0300

    Update and delete seller

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string msg) : base(msg)
+        {
+        }
+    }
+}
